71dbb60b7f0b60fd0271d63abed5f2649ad28500
yale.zheng <yale.zheng@blockc.pro>
2019-02-21 16:41:53 +0800

    fix fail to run test

--- src/keygen.test.js
+++ src/keygen.test.js
@@ -8,16 +8,18 @@ const Keygen = require('./keygen')
 describe('Keygen', () => {
 	it('initialize', () => PrivateKey.initialize())
 
-	it('generateMasterKeys (create)', async () => {
-		const keys = await Keygen.generateMasterKeys()
-		checkKeySet(keys)
+	it('generateMasterKeys (create)', () => {
+		Keygen.generateMasterKeys().then(keys => {
+			checkKeySet(keys)
+		})
 	}).timeout(5000)
 
-	it('generateMasterKeys (re-construct)', async () => {
+	it('generateMasterKeys (re-construct)', () => {
 		const master = 'PW5JMx76CTUTXxpAbwAqGMMVzSeJaP5UVTT5c2uobcpaMUdLAphSp'
-		const keys = await Keygen.generateMasterKeys(master)
-		assert.equal(keys.masterPrivateKey, master, 'masterPrivateKey')
-		checkKeySet(keys)
+		Keygen.generateMasterKeys(master).then(keys => {
+			assert.equal(keys.masterPrivateKey, master, 'masterPrivateKey')
+			checkKeySet(keys)
+		})
 	})
 
 	it('authsByPath', () => {
@@ -25,20 +27,21 @@ describe('Keygen', () => {
 		assert.deepEqual(['active', 'active/mypermission', 'owner'], Object.keys(paths))
 	})
 
-	it('deriveKeys', async () => {
+	it('deriveKeys', () => {
 		const master = 'PW5JMx76CTUTXxpAbwAqGMMVzSeJaP5UVTT5c2uobcpaMUdLAphSp'
-		const keys = await Keygen.generateMasterKeys(master)
-		const wifsByPath = {
-			owner: keys.privateKeys.owner,
-			active: keys.privateKeys.active
-		}
+		Keygen.generateMasterKeys(master).then(keys => {
+			const wifsByPath = {
+				owner: keys.privateKeys.owner,
+				active: keys.privateKeys.active
+			}
 
-		const derivedKeys = Keygen.deriveKeys('active/mypermission', wifsByPath)
-		const active = PrivateKey(keys.privateKeys.active)
-		const checkKey = active.getChildKey('mypermission').toWif()
+			const derivedKeys = Keygen.deriveKeys('active/mypermission', wifsByPath)
+			const active = PrivateKey(keys.privateKeys.active)
+			const checkKey = active.getChildKey('mypermission').toWif()
 
-		assert.equal(derivedKeys.length, 1, 'derived key count')
-		assert.equal(derivedKeys[0].path, 'active/mypermission')
-		assert.equal(derivedKeys[0].privateKey.toWif(), checkKey, 'wrong private key')
+			assert.equal(derivedKeys.length, 1, 'derived key count')
+			assert.equal(derivedKeys[0].path, 'active/mypermission')
+			assert.equal(derivedKeys[0].privateKey.toWif(), checkKey, 'wrong private key')
+		})
 	})
 })
